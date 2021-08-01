0ed5a5e07d45eb219cc54c20b59d7988d19e9e23
astrla <fabian.trigo@alumnos.uv.cl>
2021-08-01 01:28:38 -0400

    esqueleto
    por ahora creo las funciones base, para seguirlas despues de dormir

---
+++ convenios.py
@@ -0,0 +1,16 @@
+import scipy
+import numpy
+
+# errores = [0.1 , 0.2 , 0.1, 1]
+# medicion = 10
+# ddf_i = [1, 2, 3, 4]
+
+def cuad_sqrt(errores, derivadas, medicion):
+    # e_i : error numero i
+    # ddf_i : funcion diferenciada por i, evaluada en el punto
+    # de la medicion
+     #
+    # Error = sqrt( sumatoria_i f_i e_i  )
+   pass 
+
+print("funcion importada")
---
+++ main.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import scipy
+import convenios
+def main():
+  print("Hello World!")
+
+print("debug code")
+if __name__ == '__main__':
+  main()
